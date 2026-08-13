56090f6229c0a65653118ad2a888984658397729
Tharaka Jayampathi <TharakaJayam@brownsgroup.com>
2026-08-13 18:26:04 +0530

    Changes Added

---
+++ Controllers/ChequePrintController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChequePrint.Controllers
+{
+    public class ChequePrintController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
