f4443c1d35b6d6382609795e9ad24b9a6afdf4c1
Tharaka Jayampathi <TharakaJayam@brownsgroup.com>
2026-08-14 10:47:53 +0530

    Changes Added

--- Program.cs
+++ Program.cs
@@ -1,33 +1,27 @@
 using ChequePrint.Interfaces.ChequePrint;
 using ChequePrint.Repository.ChequePrint;
+using System.Text;
+
+Encoding.RegisterProvider(CodePagesEncodingProvider.Instance);
 
 var builder = WebApplication.CreateBuilder(args);
-
 // Add services to the container.
 builder.Services.AddControllersWithViews();
-
 // Register your repository
 builder.Services.AddScoped<IChequePrintRepository, ChequePrintRepository>();
-
 var app = builder.Build();
-
 // Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Home/Error");
     app.UseHsts();
 }
-
 app.UseHttpsRedirection();
 app.UseRouting();
-
 app.UseAuthorization();
-
 app.MapStaticAssets();
-
 app.MapControllerRoute(
     name: "default",
     pattern: "{controller=Auth}/{action=Index}/{id?}")
     .WithStaticAssets();
-
 app.Run();
