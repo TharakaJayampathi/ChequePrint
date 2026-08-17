2810cbaa3c285474843dc5168a641cf7887b13c2
Tharaka Jayampathi <TharakaJayam@brownsgroup.com>
2026-08-17 18:27:30 +0530

    Changes Added

--- Repository/ChequePrint/ChequePrintRepository.cs
+++ Repository/ChequePrint/ChequePrintRepository.cs
@@ -22,30 +22,27 @@ namespace ChequePrint.Repository.ChequePrint
         {
             try
             {
-                var transactionOptions = new TransactionOptions { Timeout = TimeSpan.FromMinutes(5), IsolationLevel = System.Transactions.IsolationLevel.ReadCommitted };
-                using (var transactionScope = new TransactionScope(TransactionScopeOption.Required, transactionOptions, TransactionScopeAsyncFlowOption.Enabled))
-                {
-                    var yearDigits = model.Date.Year.ToString("D4");
-                    var monthDigits = model.Date.Month.ToString("D2");
-                    var dayDigits = model.Date.Day.ToString("D2");
+                var yearDigits = model.Date.Year.ToString("D4");
+                var monthDigits = model.Date.Month.ToString("D2");
+                var dayDigits = model.Date.Day.ToString("D2");
 
-                    string Year1 = yearDigits[0].ToString();
-                    string Year2 = yearDigits[1].ToString();
-                    string Year3 = yearDigits[2].ToString();
-                    string Year4 = yearDigits[3].ToString();
-                    string Month1 = monthDigits[0].ToString();
-                    string Month2 = monthDigits[1].ToString();
-                    string Date1 = dayDigits[0].ToString();
-                    string Date2 = dayDigits[1].ToString();
+                string Year1 = yearDigits[0].ToString();
+                string Year2 = yearDigits[1].ToString();
+                string Year3 = yearDigits[2].ToString();
+                string Year4 = yearDigits[3].ToString();
+                string Month1 = monthDigits[0].ToString();
+                string Month2 = monthDigits[1].ToString();
+                string Date1 = dayDigits[0].ToString();
+                string Date2 = dayDigits[1].ToString();
 
-                    var amountDecimal = Convert.ToDecimal(model.Amount);
-                    string Amount = amountDecimal.ToString("N2", CultureInfo.InvariantCulture);
-                    string _amountInWord = ConvertAmountToWords(amountDecimal);
+                var amountDecimal = Convert.ToDecimal(model.Amount);
+                string Amount = amountDecimal.ToString("N2", CultureInfo.InvariantCulture);
+                string _amountInWord = ConvertAmountToWords(amountDecimal);
 
-                    string mimetypeCheckPrint = "";
-                    int extensionCheckPrint = 1;
+                string mimetypeCheckPrint = "";
+                int extensionCheckPrint = 1;
 
-                    var checkPrintLetterDetail = new List<CheckPrintDataSetDTO> {
+                var checkPrintLetterDetail = new List<CheckPrintDataSetDTO> {
                         new CheckPrintDataSetDTO {
                             EmployeeName = model.ChequeName,
                             Amount = Amount,
@@ -61,26 +58,21 @@ namespace ChequePrint.Repository.ChequePrint
                         }
                     };
 
-                    var reportRdlcPath = $"{_hostingEnvironment.WebRootPath}\\Report\\ChequePrint\\ChequePrint.rdlc";
+                var reportRdlcPath = $"{_hostingEnvironment.WebRootPath}\\Report\\ChequePrint\\ChequePrint.rdlc";
 
-                    Dictionary<string, string> para = new Dictionary<string, string>();
-                    para.Add("prm", "RDLC Report");
+                Dictionary<string, string> para = new Dictionary<string, string>();
+                para.Add("prm", "RDLC Report");
 
-                    LocalReport rpt = new LocalReport(reportRdlcPath);
-                    rpt.AddDataSource("dsChequePrint", checkPrintLetterDetail);
+                LocalReport rpt = new LocalReport(reportRdlcPath);
+                rpt.AddDataSource("dsChequePrint", checkPrintLetterDetail);
 
-                    var reportResultLetter = rpt.Execute(RenderType.Pdf, extensionCheckPrint, para, mimetypeCheckPrint);
+                var reportResultLetter = rpt.Execute(RenderType.Pdf, extensionCheckPrint, para, mimetypeCheckPrint);
 
-                    transactionScope.Complete();
+                var safeName = string.Join("_", model.ChequeName.Split(Path.GetInvalidFileNameChars()));
+                var dateForFileName = model.Date.ToString("yyyy_MM_dd");
+                var fileName = $"Cheque_{safeName}_{dateForFileName}.pdf";
 
-                    // Generate filename
-                    var safeName = string.Join("_", model.ChequeName.Split(Path.GetInvalidFileNameChars()));
-                    var dateForFileName = model.Date.ToString("yyyy_MM_dd");
-                    var fileName = $"Cheque_{safeName}_{dateForFileName}.pdf";
-
-                    // Return both the PDF content and the filename
-                    return (reportResultLetter.MainStream, fileName);
-                }
+                return (reportResultLetter.MainStream, fileName);
             }
             catch (Exception ex)
             {
