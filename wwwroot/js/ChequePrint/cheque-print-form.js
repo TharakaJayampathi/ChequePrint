5efbd016cfdbe362639ddf8675963e1a77536154
Tharaka Jayampathi <TharakaJayam@brownsgroup.com>
2026-08-17 15:22:34 +0530

    Changes Added

--- wwwroot/js/ChequePrint/cheque-print-form.js
+++ wwwroot/js/ChequePrint/cheque-print-form.js
@@ -3,6 +3,22 @@
     $("#selectPaymentMethod").val(1).trigger('change.select2');
 })
 
+$('#txtAmount').on('input', function () {
+    let value = this.value;
+    const regex = /^\d*(\.\d{0,2})?$/;
+    if (regex.test(value)) {
+        $(this).data('lastValidValue', value);
+    } else {
+        this.value = $(this).data('lastValidValue') || '';
+    }
+});
+
+$('#txtAmount').on('keypress', function (e) {
+    if (e.key === '-') {
+        e.preventDefault();
+    }
+});
+
 $("#selectPaymentMethod").select2({
     placeholder: "-Please Select-"
 });
