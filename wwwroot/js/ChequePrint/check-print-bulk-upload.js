68b91dc7954449e5750854ccf1cd550c3ebe4d21
Tharaka Jayampathi <TharakaJayam@brownsgroup.com>
2026-08-17 18:32:37 +0530

    Changes Added

--- wwwroot/js/ChequePrint/check-print-bulk-upload.js
+++ wwwroot/js/ChequePrint/check-print-bulk-upload.js
@@ -5,78 +5,92 @@
         toastr.error("", "No file selected", { progressBar: true });
         return;
     }
-    $('#btnText').hide();
-    $('#btnSpinner').show();
-    $('#btnAddAttachment').prop('disabled', true);
-    document.getElementById("pageOverlay").style.display = "block";
-    var files = input.files;
-    for (var i = 0; i < files.length; i++) {
-        formData.append("Files", files[i]);
-    }
 
-    $.ajax({
-        url: '/api/chequeprint/cheque-print-attachment-upload',
-        type: 'POST',
-        data: formData,
-        contentType: false,
-        processData: false,
-        xhrFields: {
-            responseType: 'blob'
-        },
-        success: function (blob, status, xhr) {
-            var disposition = xhr.getResponseHeader('Content-Disposition');
-            var fileName = 'ChequePrint.zip';
-            if (disposition && disposition.indexOf('filename=') !== -1) {
-                var match = disposition.match(/filename\*?=(?:UTF-8'')?["']?([^"';]+)["']?/i);
-                if (match && match[1]) {
-                    fileName = decodeURIComponent(match[1]);
-                }
+    var title = "Are you sure you want to upload?";
+    swal({
+        title: title,
+        text: "",
+        icon: "warning",
+        buttons: ['No', 'Yes'],
+        dangerMode: true,
+    }).then(function (isConfirm) {
+        if (isConfirm) {
+            $('#btnText').hide();
+            $('#btnSpinner').show();
+            $('#btnAddAttachment').prop('disabled', true);
+            document.getElementById("pageOverlay").style.display = "block";
+            var files = input.files;
+            for (var i = 0; i < files.length; i++) {
+                formData.append("Files", files[i]);
             }
 
-            var downloadUrl = window.URL.createObjectURL(blob);
-            var a = document.createElement('a');
-            a.href = downloadUrl;
-            a.download = fileName;
-            document.body.appendChild(a);
-            a.click();
-            a.remove();
-            window.URL.revokeObjectURL(downloadUrl);
+            $.ajax({
+                url: '/api/chequeprint/cheque-print-attachment-upload',
+                type: 'POST',
+                data: formData,
+                contentType: false,
+                processData: false,
+                xhrFields: {
+                    responseType: 'blob'
+                },
+                success: function (blob, status, xhr) {
+                    var disposition = xhr.getResponseHeader('Content-Disposition');
+                    var fileName = 'ChequePrint.zip';
+                    if (disposition && disposition.indexOf('filename=') !== -1) {
+                        var match = disposition.match(/filename\*?=(?:UTF-8'')?["']?([^"';]+)["']?/i);
+                        if (match && match[1]) {
+                            fileName = decodeURIComponent(match[1]);
+                        }
+                    }
 
-            toastr.success("", "Cheque Print generated successfully", { progressBar: true });
-        },
-        error: function (jqXHR, textStatus, errorThrown) {
-            if (jqXHR.responseText) {
-                try {
-                    var errJson = JSON.parse(jqXHR.responseText);
-                    toastr.error("", errJson.message || "Something went wrong", { progressBar: true });
-                } catch (e) {
-                    toastr.error("", "Something went wrong", { progressBar: true });
-                }
-            } else if (jqXHR.responseJSON && jqXHR.responseJSON.message) {
-                toastr.error("", jqXHR.responseJSON.message, { progressBar: true });
-            } else {
-                if (jqXHR.response instanceof Blob) {
-                    var reader = new FileReader();
-                    reader.onload = function () {
+                    var downloadUrl = window.URL.createObjectURL(blob);
+                    var a = document.createElement('a');
+                    a.href = downloadUrl;
+                    a.download = fileName;
+                    document.body.appendChild(a);
+                    a.click();
+                    a.remove();
+                    window.URL.revokeObjectURL(downloadUrl);
+
+                    toastr.success("", "Cheque Print generated successfully", { progressBar: true });
+                },
+                error: function (jqXHR, textStatus, errorThrown) {
+                    if (jqXHR.responseText) {
                         try {
-                            var errJson = JSON.parse(reader.result);
+                            var errJson = JSON.parse(jqXHR.responseText);
                             toastr.error("", errJson.message || "Something went wrong", { progressBar: true });
                         } catch (e) {
                             toastr.error("", "Something went wrong", { progressBar: true });
                         }
-                    };
-                    reader.readAsText(jqXHR.response);
-                } else {
-                    toastr.error("", "Something went wrong", { progressBar: true });
+                    } else if (jqXHR.responseJSON && jqXHR.responseJSON.message) {
+                        toastr.error("", jqXHR.responseJSON.message, { progressBar: true });
+                    } else {
+                        if (jqXHR.response instanceof Blob) {
+                            var reader = new FileReader();
+                            reader.onload = function () {
+                                try {
+                                    var errJson = JSON.parse(reader.result);
+                                    toastr.error("", errJson.message || "Something went wrong", { progressBar: true });
+                                } catch (e) {
+                                    toastr.error("", "Something went wrong", { progressBar: true });
+                                }
+                            };
+                            reader.readAsText(jqXHR.response);
+                        } else {
+                            toastr.error("", "Something went wrong", { progressBar: true });
+                        }
+                    }
+                },
+                complete: function () {
+                    $('#btnText').show();
+                    $('#btnSpinner').hide();
+                    $('#btnAddAttachment').prop('disabled', false);
+                    document.getElementById("pageOverlay").style.display = "none";
+                    document.getElementById('txtAttachment').value = '';
                 }
-            }
-        },
-        complete: function () {
-            $('#btnText').show();
-            $('#btnSpinner').hide();
-            $('#btnAddAttachment').prop('disabled', false);
-            document.getElementById("pageOverlay").style.display = "none";
-            document.getElementById('txtAttachment').value = '';
+            });
+        } else {
+            swal("Cancelled", "", "error");
         }
     });
 });
