e8d8126aecf1c8d277fabfa78bdd226d3e193139
Tharaka Jayampathi <TharakaJayam@brownsgroup.com>
2026-08-19 21:58:51 +0530

    Changes Added

---
+++ Controllers/CheckPrintReportController.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ChequePrint.Controllers
+{
+    public class CheckPrintReportController : Controller
+    {
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
