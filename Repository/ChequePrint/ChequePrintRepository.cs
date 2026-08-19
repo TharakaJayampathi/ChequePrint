eb54b841817fd8783e5ae5115fc98a02c3d57b19
Tharaka Jayampathi <TharakaJayam@brownsgroup.com>
2026-08-20 01:21:10 +0530

    Changes Added

--- Repository/ChequePrint/ChequePrintRepository.cs
+++ Repository/ChequePrint/ChequePrintRepository.cs
@@ -69,7 +69,7 @@ namespace ChequePrint.Repository.ChequePrint
 
                 var checkPrintLetterDetail = new List<CheckPrintDataSetDTO> {
                     new CheckPrintDataSetDTO {
-                        EmployeeName = model.ChequeName,
+                        EmployeeName = "",
                         Amount = Amount,
                         Year1 = Year1,
                         Year2 = Year2,
@@ -79,7 +79,8 @@ namespace ChequePrint.Repository.ChequePrint
                         Month2 = Month2,
                         Date1 = Date1,
                         Date2 = Date2,
-                        AmountInWord = _amountInWord
+                        AmountInWord = _amountInWord,
+                        ChequeName = model.ChequeName
                     }
                 };
 
