30a5a92594ce5fec42d524e3aa049aa3730141c0
Tharaka Jayampathi <TharakaJayam@brownsgroup.com>
2026-08-19 22:29:10 +0530

    Changes Added

--- Controllers/api/ChequePrintReportController.cs
+++ Controllers/api/ChequePrintReportController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using ChequePrint.DTOs.ChequePrint;
+using ChequePrint.Interfaces.ChequePrint;
 using Microsoft.AspNetCore.Mvc;
 
 namespace ChequePrint.Controllers.api
@@ -7,5 +8,40 @@ namespace ChequePrint.Controllers.api
     [ApiController]
     public class ChequePrintReportController : ControllerBase
     {
+        private readonly IChequePrintRepository _chequePrintRepository;
+
+        public ChequePrintReportController(
+            IChequePrintRepository chequePrintRepository)
+        {
+            _chequePrintRepository = chequePrintRepository;
+        }
+
+        [HttpPost("cheque-print")]
+        public async Task<IActionResult> ChequePrintAsync(CheckPrintDTO model)
+        {
+            try
+            {
+                var (content, fileName) = await _chequePrintRepository.ChequePrintAsync(model);
+                return File(content, "application/pdf", fileName);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
+
+        [HttpPost("cheque-print-attachment-upload")]
+        public async Task<IActionResult> ChequePrintAttachmentUploadAsync([FromForm] ChequePrintAttachmentUploadDTO model)
+        {
+            try
+            {
+                var (content, fileName) = await _chequePrintRepository.ChequePrintAttachmentUploadAsync(model);
+                return File(content, "application/zip", fileName);
+            }
+            catch (Exception ex)
+            {
+                return BadRequest(new { message = ex.Message });
+            }
+        }
     }
 }
